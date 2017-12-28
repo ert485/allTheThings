f05bb58947d0942a31a533fa0a20bf8603376920
Erik Tetland <ert485@mail.usask.ca>
2017-12-28 01:35:13 +0000

    fixed indents

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -15,47 +15,47 @@ class ImagesController extends Controller
     {
         $this->middleware('auth');
     }
- // Directory to look for image files
-  private $imageDir = '/home/cabox/workspace/images/';
+    // Directory to look for image files
+    private $imageDir = '/home/cabox/workspace/images/';
   
-  // Known types of images and their meta type
-  private $imageTypes = [
-      'gif'=> 'image/gif',
-      'png'=> 'image/png',
-      'jpeg'=> 'image/jpeg',
-      'jpg'=> 'image/jpeg',
+    // Known types of images and their meta type
+    private $imageTypes = [
+        'gif'=> 'image/gif',
+        'png'=> 'image/png',
+        'jpeg'=> 'image/jpeg',
+        'jpg'=> 'image/jpeg',
     ];
-  
-  // Get filename extension
-  private function getExtension($name){
-    $extension = explode(".", $name);
-    $extension = $extension[count($extension) - 1];
-    return $extension;
-  }
-  
-  // Look up image meta type
-  private function getType($name){
-    $extension = $this->getExtension($name);
-    $types = $this->imageTypes;
-    foreach($types as $type=>$meta){
-      if( $extension==$type ){
-        return $meta;
-      }
+    
+    // Get filename extension
+    private function getExtension($name){
+        $extension = explode(".", $name);
+        $extension = $extension[count($extension) - 1];
+        return $extension;
     }
+    
+    // Look up image meta type
+    private function getType($name){
+        $extension = $this->getExtension($name);
+        $types = $this->imageTypes;
+        foreach($types as $type=>$meta){
+            if( $extension==$type ){
+                return $meta;
+            }
+        }
     // if type is not found:
     return '';
-  }
-  
-  public function index($imageName){
-    $imagePath = $this->imageDir . $imageName;
-    $imageType = $this->getType($imageName);
-    if (!$imageType) echo "error getting image type";
-    else if( file_exists($imagePath) ){
-      header( "Content-type: " . $imageType );
-      header( "Content-Length: " . filesize($imagePath));
-      readfile($imagePath);
-      return;
     }
-    else echo "image not found";
-  }
+    
+    public function index($imageName){
+        $imagePath = $this->imageDir . $imageName;
+        $imageType = $this->getType($imageName);
+        if (!$imageType) echo "error getting image type";
+        else if( file_exists($imagePath) ){
+            header( "Content-type: " . $imageType );
+            header( "Content-Length: " . filesize($imagePath));
+            readfile($imagePath);
+            return;
+        }
+        else echo "image not found";
+    }
 }
